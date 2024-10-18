2edbdcfacd1bfe445db90c98b7d47b5de5d60ff4
roman alekseev <roma.alekseev.90@bk.ru>
2024-10-18 21:43:20 +0300

    create: add project page

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -4,7 +4,7 @@ const Project = ({ project }) => {
   return (
     <li className="project">
       <a href={project.gitHubLink}>
-        <img src={project.img} alt="Project img" className="project__img" />
+        <img src={project.img} alt={project.title} className="project__img" />
         <h3 className="project__title">{project.title}</h3>
       </a>
     </li>
